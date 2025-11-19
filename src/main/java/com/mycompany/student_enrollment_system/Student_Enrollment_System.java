2a49342a8e61710d08ec7319c7bf3fda27aa8722
cy.commits/ <ckbdeguzman@gmail.com>
2025-11-19 12:15:16 +0800

    Initial project upload

---
+++ src/main/java/com/mycompany/student_enrollment_system/Student_Enrollment_System.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.student_enrollment_system;
+
+/**
+ *
+ * @author Lenovo x270
+ */
+public class Student_Enrollment_System {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
